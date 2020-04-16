9cb490bf693ee8a36fc050290d6f45fb1a31fd45
v.gsteiger@unibas.ch <v.gsteiger@unibas.ch>
2020-04-16 12:33:56 +0200

    Minor code cleanup

--- groups/07-logStore/src/logStore/tests/testSqLiteConnector.py
+++ groups/07-logStore/src/logStore/tests/testSqLiteConnector.py
@@ -1,31 +1,31 @@
-from logStore_pkg import SqLiteConnector
+from logStore_pkg.SqLiteConnector import SqLiteConnector
 
 def main():
-    connector = SqLiteConnector.SqLiteConnector
-    connector.name_database(connector, 'TestDatabase')
-    connector.start_database_connection(connector)
-    connector.create_table(connector, 'chat')
-    connector.insert_to_table(connector, 'chat', '20:45', 'Peter', 'Hallo')
-    connector.insert_to_table(connector, 'chat', '20:46', 'Max', 'Hallo')
-    connector.insert_to_table(connector, 'chat', '20:47', 'Gustavo', 'Hallo')
+    connector = SqLiteConnector()
+    connector.name_database('TestDatabase')
+    connector.start_database_connection()
+    connector.create_table('chat')
+    connector.insert_to_table('chat', '20:45', 'Peter', 'Hallo')
+    connector.insert_to_table('chat', '20:46', 'Max', 'Hallo')
+    connector.insert_to_table('chat', '20:47', 'Gustavo', 'Hallo')
 
-    connector.get_all_from_table(connector, 'chat')
+    connector.get_all_from_table('chat')
     rows = connector.cursor.fetchall()
 
     for row in rows:
         print(row)
 
     print("\n")
-    connector.search_in_table(connector, 'chat', 'Peter')
+    connector.search_in_table('chat', 'Peter')
 
     rows = connector.cursor.fetchall()
     print(rows)
 
-    connector.commit_changes(connector)
+    connector.commit_changes()
 
-    connector.close_table(connector, 'chat')
+    connector.close_table('chat')
 
-    connector.close_database_connection(connector)
+    connector.close_database_connection()
 
 
 
