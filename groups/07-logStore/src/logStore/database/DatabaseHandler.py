5418f99941ded348f1c3ca8392e937600b489b7c
v.gsteiger@unibas.ch <v.gsteiger@unibas.ch>
2020-04-30 15:52:10 +0200

    Updated readme and worked on byteArray retrieval logic

--- groups/07-logStore/src/logStore/database/DatabaseHandler.py
+++ groups/07-logStore/src/logStore/database/DatabaseHandler.py
@@ -9,10 +9,10 @@ class DatabaseHandler:
         self.__eventHandler = EventHandler()
 
     def add_to_db(self, byteArray):
-        pass
+        self.__byteArrayHandler.insert_byte_array(byteArray)
 
     def get_byte_array(self, hashref):
-        pass
+        return self.__byteArrayHandler.retrieve_byte_array(hashref)
 
     def verify_byte_array(self, byteArray):
         return False
