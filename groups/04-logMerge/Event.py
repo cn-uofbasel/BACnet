26a0bb8e6a26fea44fca5de9db2ad87362faea3e
NikodemKch <nikodem.kch@gmail.com>
2020-05-04 20:11:14 +0200

    G4: Added all basic functionality

--- groups/04-logMerge/Event.py
+++ groups/04-logMerge/Event.py
@@ -4,9 +4,6 @@ import hashlib
 
 class Meta:
 
-    def __init__(self, meta):
-        self.feed_id, self.seq_no, self.hash_of_prev, self.signature_info, self.hash_of_content = cbor2.loads(meta)
-
     def __init__(self, feed_id, seq_no, hash_of_prev, signature_info, hash_of_content):
         self.feed_id = feed_id
         self.seq_no = seq_no
@@ -14,17 +11,24 @@ class Meta:
         self.signature_info = signature_info
         self.hash_of_content = hash_of_content
 
+    @classmethod
+    def from_cbor(cls, meta):
+        feed_id, seq_no, hash_of_prev, signature_info, hash_of_content = cbor2.loads(meta)
+        return Meta(feed_id, seq_no, hash_of_prev, signature_info, hash_of_content)
+
     def get_as_cbor(self):
         return cbor2.dumps([self.feed_id, self.seq_no, self.hash_of_prev, self.signature_info, self.hash_of_content])
 
 
 class Content:
 
-    def __init__(self, content):
-        self.content = cbor2.loads(content)
-
     def __init__(self, identifier, parameter):
         self.content = [identifier, parameter]
+
+    @classmethod
+    def from_cbor(cls, content):
+        identifier, parameters = cbor2.loads(content)
+        return Content(identifier, parameters)
 
     def get_as_cbor(self):
         return cbor2.dumps(self.content)
@@ -32,16 +36,19 @@ class Content:
 
 class Event:
 
-    # parameter event: cbor encoded feed event
-    def __init__(self, event):
-        meta, self.signature, content = cbor2.loads(event)
-        self.meta = Meta(meta)
-        self.content = Content(content)
-
     def __init__(self, meta, signature, content):
         self.meta = meta
         self.signature = signature
         self.content = content
+
+    # parameter event: cbor encoded feed event
+    @classmethod
+    def from_cbor(self, event):
+        meta, signature, content = cbor2.loads(event)
+        meta = Meta.from_cbor(meta)
+        content = Content.from_cbor(content)
+        return Event(meta, signature, content)
+
 
     def get_as_cbor(self):
         return cbor2.dumps([self.meta.get_as_cbor(), self.signature, self.content.get_as_cbor()])
