f2d79157aaf6c35ba3e3e67ad3c0aa35b5f939c3
Moritz <moritz.wuerth@stud.unibas.ch>
2020-05-06 11:09:28 +0200

    add  some connections to the database

---
+++ groups/07-logStore/src/logStore/upConnection/Function.py
@@ -0,0 +1,22 @@
+from logStore.database.DatabaseHandler import DatabaseHandler
+
+
+class Function:
+
+    def __init__(self):
+        self.__handler = DatabaseHandler()
+
+    def get_feed_ids(self):
+        pass
+
+    def insert_event(self, Event):
+        pass
+
+    def get_event(self, feedId, Hash):
+        pass
+
+    def get_all_events_since(self, feedId, Hash):
+        pass
+
+    def get_full_feed(self, feedId):
+        pass
