39412f8563bb5f23702df6aac82fdd98e386476f
NikodemKch <nikodem.kch@gmail.com>
2020-05-07 13:07:30 +0200

    finished tutorial

--- groups/04-logMerge/Event.py
+++ groups/04-logMerge/Event.py
@@ -1,9 +1,9 @@
 import cbor2
-import hashlib
 
 
 class Meta:
 
+    # Create the Meta() object from scratch (for example if you create a new event)
     def __init__(self, feed_id, seq_no, hash_of_prev, signature_info, hash_of_content):
         self.feed_id = feed_id
         self.seq_no = seq_no
@@ -12,30 +12,32 @@ class Meta:
         self.hash_of_content = hash_of_content
 
     @classmethod
-    def from_cbor(cls, meta):
+    def from_cbor(cls, meta):  # Read in a Meta() object from cbor format
         feed_id, seq_no, hash_of_prev, signature_info, hash_of_content = cbor2.loads(meta)
         return Meta(feed_id, seq_no, hash_of_prev, signature_info, hash_of_content)
 
-    def get_as_cbor(self):
+    def get_as_cbor(self):  # Get the cbor encoded version of the meta object
         return cbor2.dumps([self.feed_id, self.seq_no, self.hash_of_prev, self.signature_info, self.hash_of_content])
 
 
 class Content:
 
-    def __init__(self, identifier, parameter):
-        self.content = [identifier, parameter]
+    # create content from scratch from identifier and parameter dictionary as specified in BACnet documentation
+    def __init__(self, identifier, parameters):
+        self.content = [identifier, parameters]
 
     @classmethod
-    def from_cbor(cls, content):
+    def from_cbor(cls, content):  # Read in a Content() object from cbor format
         identifier, parameters = cbor2.loads(content)
         return Content(identifier, parameters)
 
-    def get_as_cbor(self):
+    def get_as_cbor(self):  # Get the Content cbor encoded (as bytes() python object)
         return cbor2.dumps(self.content)
 
 
 class Event:
 
+    # Create Event from scratch. NOTE!!!: meta and content parameters must be Meta() and Content() objects! (see above)
     def __init__(self, meta, signature, content):
         self.meta = meta
         self.signature = signature
@@ -43,23 +45,11 @@ class Event:
 
     # parameter event: cbor encoded feed event
     @classmethod
-    def from_cbor(self, event):
+    def from_cbor(self, event):  # Read in an Event from cbor format (parameter is bytes()), creates a new Event object
         meta, signature, content = cbor2.loads(event)
         meta = Meta.from_cbor(meta)
         content = Content.from_cbor(content)
         return Event(meta, signature, content)
 
-
-    def get_as_cbor(self):
+    def get_as_cbor(self):  # return an event cbor encoded as bytes() python object
         return cbor2.dumps([self.meta.get_as_cbor(), self.signature, self.content.get_as_cbor()])
-
-
-if __name__ == "__main__":
-    content = Content('whateverapp/whateveraction', {'somekey': 'somevalue', 'someotherkey': 753465734265})
-    hoc = hashlib.sha256(content.get_as_cbor()).hexdigest()
-    signature = '832649741acddcf4382975ccd89d98af987ab9b90e908fff9ae89bac8bacbac1'
-    meta = Meta('7326acd76a876adc89e78897d8778f78787c879234322acd789d12caffff5da2', 0, None, 'sha256', hoc)
-    event = Event(meta, signature, content)
-    file = open('dummyfeedentry.cbor', 'wb')
-    file.write(event.get_as_cbor())
-    file.close()
