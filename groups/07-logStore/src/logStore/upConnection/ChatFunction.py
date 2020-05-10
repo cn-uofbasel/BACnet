b8df4b26c79aef9ab5046bf3b82b2b02ac966d1a
Moritz <moritz.wuerth@stud.unibas.ch>
2020-05-10 16:15:16 +0200

    add prototype of the chat interface

--- groups/07-logStore/src/logStore/upConnection/ChatFunction.py
+++ groups/07-logStore/src/logStore/upConnection/ChatFunction.py
@@ -3,11 +3,13 @@ from .Function import Function
 
 class Chatfunction(Function):
 
-    def insert_chat_msg(self, byteObj):
-        return False
+    def insert_chat_msg(self, cbor):
+        self.__handler.add_to_db(event_as_cbor=cbor)
 
-    def get_chat_since(self, chatId, msgId):
-        pass
+    # example 'chat/chat_Id'    ->  'chat/01'
+    def get_chat_since(self, application, timestamp):
+        result = self.__handler.get_event_since(self, application, timestamp)
+        return result
 
-    def get_full_chat(self, chatId):
-        pass
+    def get_full_chat(self, application):
+        return self.__handler.get_all_from_application(application)
