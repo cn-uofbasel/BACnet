3aba4beb89f38604538e752e4cf6fdf5904ea816
Moritz <moritz.wuerth@stud.unibas.ch>
2020-05-10 16:15:16 +0200

    add prototype of the chat interface

--- groups/07-logStore/src/logStore/database/EventHandler.py
+++ groups/07-logStore/src/logStore/database/EventHandler.py
@@ -1,23 +1,33 @@
 from functions.Singleton import Singleton
 from .SqlAlchemyConnector import SqLiteDatabase
 from functions.Constants import SQLITE
+from functions import Event
 
 
 class EventHandler(metaclass=Singleton):
 
     def __init__(self):
-        # self.__sqlAlchemyConnector = SqLiteDatabase(SQLITE, dbname='cborDatabase.sqlite')
-        # self.__sqlAlchemyConnector.create_db_tables()
-        pass
+        self.__sqlAlchemyConnector = SqLiteDatabase(SQLITE, dbname='eventDatabase.sqlite')
+        self.__sqlAlchemyConnector.create_event_table()
 
     def init_event_table(self):
         return False
 
-    def add_event(self, byteArray):
-        return False
+    def add_event(self, event_as_cbor):
+        event = Event.from_cbor(event_as_cbor)
+        seq_no = event.meta.seq_no
+        feed_id = event.meta.feed_id.decode()
+        # TODO: fix problem with the timestamp in content
+        application, timestamp, content = Event.Content.from_cbor(event)
 
-    def get_event(self, hashref):
-        pass
+        self.__sqlAlchemyConnector.insert_event(feed_id=feed_id, seq_no=seq_no, application=application,
+                                                timestamp=timestamp, data=content)
+
+    def get_event_since(self, application, timestamp):
+        return self.__sqlAlchemyConnector.get_all_events_since(self, application, timestamp)
+
+    def get_all_events(self, application):
+        return self.__sqlAlchemyConnector.get_all_event_from_application(application)
 
     def create_table_for_feed(self, feedId):
         pass
