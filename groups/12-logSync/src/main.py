8377eca282a7c4c55ffc510331c237e12695b2ac
Carlos Tejera <carlos.tejer@stud.unibas.ch>
2020-05-11 21:49:48 +0200

    Add main.py
    Dumps files of two given directories and also syncs them

---
+++ groups/12-logSync/src/main.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import pcap
+from sync import Sync
+
+
+def check_dir(dir1):
+    if not os.path.isdir(dir1[0]) or not os.path.isdir(dir1[1]):
+        print("Directories do not exist")
+        sys.exit()
+
+
+def create_list_of_files(dir1):
+    dir_list = os.listdir(dir1)
+
+    l = []
+    for n, elem in enumerate(dir_list):
+        file = dir1 + elem
+        fid1, seq1 = pcap.get_fid_and_seq(file)
+        l.append([file, fid1])
+    return l
+
+
+def sync_directories():
+    check_dir(args.sync)
+
+    list1 = create_list_of_files(args.sync[0])
+    list2 = create_list_of_files(args.sync[1])
+
+    print("sync...")
+
+    new_file_list1 = []
+    for i in list1:
+        found = False
+        for j in list2:
+            file1, key1 = i[:2]
+            file2, key2 = j[:2]
+            if key1 == key2:
+                found = True
+                synchro = Sync(file1, file2)
+                # Only syncs if files are not up-to-date
+                if not synchro.up_to_date:
+                    synchro.sync_files()
+                # Pops element of the second list if match was found to avoid unnecessary iterations
+                list2.pop(list2.index(j))
+                continue
+        # If no match was found for the element of the first list, that means that the file is new and needs to be
+        # added to the second list, respectively second directory. This is done below.
+        if not found:
+            new_file_list1.append(i)
+    print("finished")
+
+    print(new_file_list1)
+    print(list2)
+
+
+def dump_directories_cont():
+    check_dir(args.dump)
+    list1 = create_list_of_files(args.dump[0])
+    list2 = create_list_of_files(args.dump[1])
+    for i in list1:
+        print(i[0] + ':')
+        pcap.dump(i[0])
+        print("-------------------------------")
+
+    print('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+
+    for i in list2:
+        print(i[0] + ':')
+        pcap.dump(i[0])
+        print("-------------------------------")
+
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(description='Tool for synchronisation')
+
+    parser.add_argument('--sync', metavar='DIR', nargs=2)
+    parser.add_argument('--dump', metavar='DIR', nargs=2)
+    args = parser.parse_args()
+
+    if args.dump is not None:
+        dump_directories_cont()
+
+    if args.sync is not None:
+        sync_directories()
