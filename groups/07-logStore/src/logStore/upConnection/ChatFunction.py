04168745ee122a3a2b35d1e187a454810b8b6057
Moritz <moritz.wuerth@stud.unibas.ch>
2020-05-12 14:42:45 +0200

    improved event filter

--- groups/07-logStore/src/logStore/upConnection/ChatFunction.py
+++ groups/07-logStore/src/logStore/upConnection/ChatFunction.py
@@ -6,11 +6,9 @@ class Chatfunction(Function):
     def insert_chat_msg(self, cbor):
         self.__handler.add_to_db(event_as_cbor=cbor)
 
-        # application = chatID???
-
-    def get_chat_since(self, application, timestamp, feed_Id):
-        result = self.__handler.get_event_since(self, application, timestamp, feed_Id)
+    def get_chat_since(self, application, timestamp, feed_Id, chat_id):
+        result = self.__handler.get_event_since(application, timestamp, feed_Id, chat_id)
         return result
 
-    def get_full_chat(self, application, feed_Id):
-        return self.__handler.get_all_from_application(application, feed_Id)
+    def get_full_chat(self, application, feed_Id, chat_id):
+        return self.__handler.get_all_from_application(application, feed_Id, chat_id)
