e9b43164adc51965c56faa9441a938b9d1bca53a
Carlos Andrés Tejera <carlos.tejera@stud.unibas.ch>
2020-05-16 16:17:38 +0200

    Add get methods for the important packets

--- groups/12-logSync/src/udp_connection.py
+++ groups/12-logSync/src/udp_connection.py
@@ -15,6 +15,8 @@ class Server:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((host, port))  # 1
 
+        self.__package_to_send_as_bytes = []
+
         print("Server started: <" + host + ">:<" + str(port) + ">")
         print('Waiting for query...')
 
@@ -38,10 +40,21 @@ class Server:
         # Server sends the log extensions one by one
         for file_info in list_with_necessary_files:
             packet = pcap.get_meta_and_cont_bits('udpDir/' + file_info[0], file_info[2])  # 10
+            self.__package_to_send_as_bytes.append(cbor2.dumps(packet))
+            #############################################################################################
             self.socket.sendto(cbor2.dumps(packet), address)  # 11
             print("Sending extensions from seq=" + str(file_info[2]) + " on of " + file_info[0] + "...")
+            #############################################################################################
 
         self.socket.close()
+
+    """
+    This is a list of the needed extensions of log files that have to be sent (already serialized)! 
+    This method can be used by the other groups to transfer the information as they have to (QR code, etc...)
+    """
+
+    def get_packet_to_send_as_bytes(self):
+        return self.__package_to_send_as_bytes
 
 
 class Client:
@@ -52,23 +65,29 @@ class Client:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 2
         self.socket.sendto(str.encode('requesting_infos_of_all_pcap_files'), address)  # 3
 
+        self.__received_package_as_events = []
+
         # Contains a list of all files of the server side and their sequence number
         requested_list = cbor2.loads(self.socket.recv(buffSize))  # 5 & 6
 
-        compared_files = sync.compare_files(requested_list)  # 7
-        self.socket.sendto(cbor2.dumps(compared_files), address)  # 8
+        self.__compared_files = sync.compare_files(requested_list)  # 7
+        self.socket.sendto(cbor2.dumps(self.__compared_files), address)  # 8
 
+        #############################################################################################
         # Client receives log extensions one by one and appends them
-        for file_info in compared_files:
-            event = cbor2.loads(self.socket.recv(buffSize))  # 11
-            synchro = sync.Sync('udpDir/' + file_info[0])
+        for file_info in self.__compared_files:
+            event = self.socket.recv(buffSize)  # 11
+            self.__received_package_as_events.append(event)
 
-            # If the file has to be created, the key is needed
-            if file_info[2] == 0:
-                synchro.sync_files(key=file_info[1], new_files=True, event_list=event)  # 12
-            else:
-                synchro.sync_files(event_list=event)  # 12
-            print("Synchronising " + file_info[0] + "...")
-
-        print("Finished synchronising!")
+        print("Packets received!")
+        #############################################################################################
         self.socket.close()
+
+    """
+    This is a list of the received log extensions that can be appended to the files.
+    """
+    def get_packet_to_receive_as_bytes(self):
+        return self.__received_package_as_events
+
+    def get_compared_files(self):
+        return self.__compared_files
