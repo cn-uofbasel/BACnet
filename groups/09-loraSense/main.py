0c5e62e6676cb58839e68c313a3df06a4d49deed
Leonardo Salsi <leonardo.salsi@stud.unibas.ch>
2020-05-18 15:56:29 +0200

    Debug mode added. User can now specify a timeout for the WLAN connection.

--- groups/09-loraSense/main.py
+++ groups/09-loraSense/main.py
@@ -1,6 +1,6 @@
 import lib.lorasense as lorasense
 
-lorasense = lorasense.LoraSense(mode=0, debug=0)
+lorasense = lorasense.LoraSense(mode=0, debug=1)
 lorasense.setupLoRa()
 lorasense.setupWLAN("CasaSalsi","S@lsi1968")
 lorasense.startLoRaComm()
