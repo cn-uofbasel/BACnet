f4bad1f696a75ee2bfe8a1f0905b3510cfc0d6f6
Carlos Andrés Tejera <carlos.tejera@stud.unibas.ch>
2020-05-18 16:35:00 +0200

    Add the functionality of UDP broadcasting

--- groups/12-logSync/src/main.py
+++ groups/12-logSync/src/main.py
@@ -86,8 +86,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--sync', metavar='DIR', nargs=2)
     parser.add_argument('--dump', metavar='DIR')
-    parser.add_argument('--server', metavar='server', nargs=2)
-    parser.add_argument('--client', metavar='client', nargs=2)
+    parser.add_argument('--server', metavar='server', nargs=1)
+    parser.add_argument('--client', metavar='client', nargs=1)
     args = parser.parse_args()
 
     if args.dump is not None:
@@ -97,12 +97,12 @@ if __name__ == '__main__':
         sync_directories(args.sync)
 
     if args.server is not None:
-        server = udp_connection.Server(args.server[0], args.server[1])
-        print(server.get_packet_to_send_as_bytes())
+        server = udp_connection.Server(args.server[0])
+        # print(server.get_packet_to_send_as_bytes())
 
     if args.client is not None:
-        client = udp_connection.Client(args.client[0], args.client[1])
-        print(client.get_packet_to_receive_as_bytes())
+        client = udp_connection.Client(args.client[0])
+        # print(client.get_packet_to_receive_as_bytes())
 
         # This is the crucial function for the other groups (Synchronisation). The client contains two important lists:
         # A list of files that are going to be extended and their corresponding extensions (groups will enter their
