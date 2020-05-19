010feade7e7d62dfbeff55162bfe1e7f805467f4
v.gsteiger@unibas.ch <v.gsteiger@unibas.ch>
2020-05-19 15:43:57 +0200

    Added stubs and fixed inheritance errors

--- groups/07-logStore/src/logStore/appconn/connection.py
+++ groups/07-logStore/src/logStore/appconn/connection.py
@@ -4,22 +4,22 @@ from ..database.database_handler import DatabaseHandler
 class Function:
 
     def __init__(self):
-        self.__handler = DatabaseHandler()
+        self._handler = DatabaseHandler()
 
     def get_feed_ids(self):
         pass
 
     def insert_event(self, cbor):
-        self.__handler.add_to_db(event_as_cbor=cbor, app=True)
+        self._handler.add_to_db(event_as_cbor=cbor, app=True)
 
-    def get_event(self, feedId, Hash):
+    def get_event(self, feed_id):
         pass
 
-    def get_all_events_since(self, feedId, Hash):
+    def get_all_events_since(self, feed_id):
         pass
 
-    def get_full_feed(self, feedId):
+    def get_full_feed(self, feed_id):
         pass
 
     def get_last_event(self, feed_id):
-        return self.__handler.get_current_event_as_cbor(feed_id)
+        return self._handler.get_current_event_as_cbor(feed_id)
--- groups/07-logStore/src/logStore/database/database_handler.py
+++ groups/07-logStore/src/logStore/database/database_handler.py
@@ -92,3 +92,42 @@ class DatabaseHandler:
 
     def get_last_kotlin_event(self):
         return self.__eventHandler.get_last_kotlin_event()
+
+    def get_trusted(self, master_id):
+        pass
+
+    def get_blocked(self, master_id):
+        pass
+
+    def get_all_master_ids(self):
+        pass
+
+    def get_all_master_ids_feed_ids(self, master_id):
+        pass
+
+    def get_username(self, master_id):
+        pass
+
+    def get_my_last_event(self):
+        pass
+
+    def get_host_master_id(self):
+        pass
+
+    def get_radius(self):
+        pass
+
+    def get_master_id_from_feed(self, feed_id):
+        pass
+
+    def get_application_name(self, feed_id):
+        pass
+
+    def get_feed_ids_from_application_in_master_id(self, master_id, application_name):
+        pass
+
+    def get_feed_ids_in_radius(self):
+        pass
+
+    def set_feed_ids_radius(self, feed_id, radius):
+        pass
