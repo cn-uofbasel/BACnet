0b084c99bc7104c14d7ea4310818429a9b77d144
v.gsteiger@unibas.ch <v.gsteiger@unibas.ch>
2020-05-19 22:52:01 +0200

    Implemented and tested all feedctrl connections
    All methods have corresponding unit tests and it is described which ones
    work and which ones don't.

--- groups/07-logStore/src/logStore/appconn/connection.py
+++ groups/07-logStore/src/logStore/appconn/connection.py
@@ -6,20 +6,8 @@ class Function:
     def __init__(self):
         self._handler = DatabaseHandler()
 
-    def get_feed_ids(self):
-        pass
-
     def insert_event(self, cbor):
         self._handler.add_to_db(event_as_cbor=cbor, app=True)
-
-    def get_event(self, feed_id):
-        pass
-
-    def get_all_events_since(self, feed_id):
-        pass
-
-    def get_full_feed(self, feed_id):
-        pass
 
     def get_last_event(self, feed_id):
         return self._handler.get_current_event_as_cbor(feed_id)
