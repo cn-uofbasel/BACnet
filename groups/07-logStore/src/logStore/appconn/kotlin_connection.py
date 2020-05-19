a67f09c993c453caf1ff17d3d31cab1a45d2d854
v.gsteiger@unibas.ch <v.gsteiger@unibas.ch>
2020-05-19 15:43:57 +0200

    Added stubs and fixed inheritance errors

--- groups/07-logStore/src/logStore/appconn/kotlin_connection.py
+++ groups/07-logStore/src/logStore/appconn/kotlin_connection.py
@@ -6,20 +6,18 @@ class KotlinFunction(Function):
 
     def __init__(self):
         super(KotlinFunction, self).__init__()
-        """keinen PLan wieso es das braucht. Nimmt sonst den Databasehandler nicht"""
-        self.__handler = DatabaseHandler()
 
     def insert_data(self, cbor):
         self.insert_event(cbor)
 
     def get_usernames_and_feed_id(self):
-        return self.__handler.get_usernames_and_feed_id()
+        return self._handler.get_usernames_and_feed_id()
 
     def get_all_entries_by_feed_id(self, feed_id):
-        return self.__handler.get_all_entries_by_feed_id(feed_id)
+        return self._handler.get_all_entries_by_feed_id(feed_id)
 
     def get_all_kotlin_events(self):
-        return self.__handler.get_all_kotlin_events()
+        return self._handler.get_all_kotlin_events()
 
     def get_last_kotlin_event(self):
-        return self.__handler.get_last_kotlin_event()
+        return self._handler.get_last_kotlin_event()
