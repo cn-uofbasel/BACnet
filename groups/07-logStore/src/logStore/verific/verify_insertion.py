b5d97343d046a1b457bcb05a416f1aa1b22f67c6
DKnuchel <damian.knuchel@gmail.com>
2020-05-22 15:39:50 +0200

    eventCreationWrapper.py: - Removed some unused methods
    ListHandler.py:
    - Removed ListHandler.py as it is not needed anymore
    
    main.py:
    - Added CLI for using the Feed Controller
    
    test_verify_insertion.py:
    - Added unittests for the Verification class.
     Currently we are testing if...
      - ... incoming master_ids is accepted.
      - ... incoming trusted feed_id is accepted.
      - ... incoming not trusted is discarded.
      - ... incoming blocked are discarded.
      - ... outgoing master is accepted.
      - ... outgoing trusted is accepted.
      - ... outgoing not trusted is discarded.
      - ... outgoing blocked is discarded.
      - ... feed_id is inside the given radius.
    
    uiFunctionsHandler.py:
    - Renamed object to a more fitting name
    - Added test data generation for testing the CLI and later on the UI.
    
    verify_insertion.py:
    - separated the check if inside radius so we can test this separately.
    - changed the structure to a more logical work flow

--- groups/07-logStore/src/logStore/verific/verify_insertion.py
+++ groups/07-logStore/src/logStore/verific/verify_insertion.py
@@ -2,25 +2,44 @@ from ..appconn.feed_ctrl_connection import FeedCtrlConnection
 
 class Verification:
     def __init__(self):
-        self._feedctrl = FeedCtrlConnection
-        _hostid = self._feedctrl.get_host_master_id()
+        self._fcc = FeedCtrlConnection()
+        self._hostid = self._fcc.get_host_master_id()
 
-    def check_incoming(self, feed_id, is_master=False):
-        if is_master:
+    def check_incoming(self, feed_id, app_name):
+        # If the given feed is a master feed we will always accept it.
+        if app_name == 'MASTER':
             return True
         else:
-            trusted = set(self._feedctrl.get_trusted(self._hostid))
-            blocked = set(self._feedctrl.get_blocked(self._hostid))
-            return feed_id in trusted and feed_id not in blocked
+            trusted = set(self._fcc.get_trusted(self._hostid))
+            blocked = set(self._fcc.get_blocked(self._hostid))
+            # check if the feedID is trusted and not blocked
+            if feed_id in trusted and feed_id not in blocked:
+                return True
+            return self._check_in_radius(feed_id, app_name)
+
 
     def check_outgoing(self, feed_id):
-        blocked = set(self._feedctrl.get_blocked(self._hostid))
+        # check if the feed_id is a master id.
+        master = set(self._fcc.get_all_master_ids())
+        blocked = set(self._fcc.get_blocked(self._hostid))
+        if feed_id in master:
+            return True
+        # check if the feed_id is not blocked
         if feed_id in blocked:
             return False
-        master = set(self._feedctrl.get_all_master_ids())
-        if (feed_id in master):
-            return True
         else:
-            trusted = set(self._feedctrl.get_trusted(self._hostid))
-            return feed_id in trusted
+            # check if the feed_id is trusted
+            trusted = set(self._fcc.get_trusted(self._hostid))
+            if feed_id in trusted:
+                return True
+            return self._check_in_radius(feed_id, self._fcc.get_application_name(feed_id))
 
+    def _check_in_radius(self, feed_id, app_name):
+        masterInRadius = set(self._fcc.get_feed_ids_in_radius())
+        # check if the feed_id is inside the social radius
+        for master in masterInRadius:
+            feedIDs = set(self._fcc.get_trusted(master))
+            for __feedID in feedIDs:
+                if app_name == self._fcc.get_application_name(__feedID):
+                    return True
+        return False
--- groups/14-feedCtrl/src/eventCreationWrapper.py
+++ groups/14-feedCtrl/src/eventCreationWrapper.py
@@ -2,44 +2,27 @@ import sys
 import os
 sys.path.append(os.path.abspath('../../04-logMerge/eventCreationTool'))
 import EventCreationTool
-from enum import Enum
-
-class Events(Enum):
-    MASTER = "MASTER/MASTER" # {}
-    Trust = "MASTER/Trust" # {'feed_id": feedID}
-    Untrust = "MASTER/Untrust" # {'feed_id', feedID}
-    Name = "MASTER/Name" # {'name', name}
-    NewFeed = "MASTER/NewFeed" # {'feed_id': feedID, 'app_name': 'TestApp'}
-    Block = "MASTER/Block" # {'feed_id': feedID}
-    Unblock = "MASTER/Unblock" # {'feed_id': feedID}
-    Radius = "MASTER/Radius" # {'radius', radius}
 
 
 class EventCreationWrapper:
 
     def __init__(self, eventFactory):
-        _eventFactory = eventFactory
+        self._eventFactory = eventFactory
 
     def create_MASTER(self):
-        return self._eventFactory.new_event(Events.MASTER, {})
+        return self._eventFactory.next_event('MASTER/MASTER', {})
 
-    def create_trust(self, feedID):
-        return self._eventFactory.new_event(Events.Trust, {'feed_id': feedID})
-
-    def create_untrust(self, feedID):
-        return self._eventFactory.new_event(Events.Untrust, {'feed_id': feedID})
+    def create_trust(self, feed_id):
+        return self._eventFactory.next_event('MASTER/Trust', {'feed_id': feed_id})
 
     def create_name(self, name):
-        return self._eventFactory.new_event(Events.Name, {'name', name})
+        return self._eventFactory.next_event('MASTER/Name', {'name': name})
 
-    def create_newFeed(self, feedID, appName):
-        return self._eventFactory.new_event(Events.NewFeed, {'feed_id': feedID, 'app_name': appName})
+    def create_newFeed(self, feed_id, appName):
+        return self._eventFactory.next_event('MASTER/NewFeed', {'feed_id': feed_id, 'app_name': appName})
 
-    def create_block(self, feedID):
-        return self._eventFactory.new_event(Events.Block, {'feed_id': feedID})
-
-    def create_unblock(self, feedID):
-        return self._eventFactory.new_event(Events.Unblock, {'feed_id': feedID})
+    def create_block(self, feed_id):
+        return self._eventFactory.next_event('MASTER/Block', {'feed_id': feed_id})
 
     def create_radius(self, radius):
-        return self._eventFactory.new_event(Events.Radius, {'radius', radius})
+        return self._eventFactory.next_event('MASTER/Radius', {'radius': radius})
