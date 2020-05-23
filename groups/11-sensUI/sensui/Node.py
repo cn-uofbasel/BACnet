cad61d037b925326d471c9214152482f67068bb6
marc_schaefer <marc-.schaefer@stud.unibas.ch>
2020-05-23 05:28:44 +0200

    Implement callbacks for tabs

--- groups/11-sensUI/sensui/Node.py
+++ groups/11-sensUI/sensui/Node.py
@@ -4,27 +4,16 @@ class Node(object):
     POSITION_ELEVATION = "Elevation"
     POSITION = [POSITION_LATITUDE, POSITION_LONGITUDE, POSITION_ELEVATION]
 
-    SENSOR_TEMPERATURE = "Temperature"
-    SENSOR_HUMIDITY = "Humidity"
-    SENSOR_PRESSURE = "Pressure"
-    SENSOR_BRIGHTNESS = "Brightness"
-    SENSORS = [SENSOR_TEMPERATURE, SENSOR_PRESSURE, SENSOR_HUMIDITY, SENSOR_BRIGHTNESS]
-
     def __init__(self, nodeId):
         self.id = nodeId
-        self.name = None
+        self.__name = None
         self.interval = 300
         self.position = {
             Node.POSITION_LATITUDE: 47.559113,
             Node.POSITION_LONGITUDE: 7.583425,
             Node.POSITION_ELEVATION: 300.
         }
-        self.sensors = {
-            Node.SENSOR_TEMPERATURE: False,
-            Node.SENSOR_PRESSURE: False,
-            Node.SENSOR_HUMIDITY: False,
-            Node.SENSOR_BRIGHTNESS: False
-        }
+        self.__sensors = {}
 
     @property
     def name(self):
@@ -36,3 +25,12 @@ class Node(object):
     def name(self, name):
         if name is not None:
             self.__name = name
+
+    def setSensor(self, id, active):
+        if active:
+            self.__sensors[id] = True
+        elif id in self.__sensors:
+            del self.__sensors[id]
+
+    def getSensors(self):
+        return self.__sensors
