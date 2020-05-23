4b029ac091aaf1807c170bb14a603365a278a252
Leonardo Salsi <leonardo.salsi@stud.unibas.ch>
2020-05-23 10:44:13 +0200

    Repository cleaned up. New folder structure in repository needed.

--- groups/09-loraSense/boot.py
+++
@@ -1,6 +0,0 @@
-##import lib.lorasense as lorasense
-
-##lorasense = lorasense.LoraSense(mode=1, debug=1)
-##lorasense.setupWLAN("CasaSalsi","S@lsi1968")
-##lorasense.setupLoRa(start=True)
-##lorasense.setupUDP("192.168.1.123",54528, start=True)
