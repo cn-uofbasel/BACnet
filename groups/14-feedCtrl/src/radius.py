9b0321ff08d9d27480328851ce5cc8293ca8a60f
v.gsteiger@unibas.ch <v.gsteiger@unibas.ch>
2020-05-23 17:57:06 +0200

    Worked on 7 and 14 integration with a new package
    Integrated the two folders and updated unit tests etc. Worked together
    with damian for 3h.

--- groups/14-feedCtrl/src/radius.py
+++ groups/14-feedCtrl/src/radius.py
@@ -6,7 +6,26 @@ from logStore.appconn.feed_ctrl_connection import FeedCtrlConnection
 class Radius:
 
     def __init__(self):
-        self._feedCtrlConnection = FeedCtrlConnection()
-        self._radius = self._feedCtrlConnection.get_radius()
-        self._hostID = self._feedCtrlConnection.get_host_master_id()
-        self._trusted = self._feedCtrlConnection.get_trusted(self._hostID)
+        self._fcc = FeedCtrlConnection()
+        self._hostID = self._fcc.get_host_master_id()
+
+    def calculate_radius(self):
+        radius = self._fcc.get_radius()
+        self.__check_trusted(self._hostID, radius, 'MASTER')
+
+    def __check_trusted(self, master_id, radius, prev_app_name, step=1, ):
+        if radius < 1 or step > radius:
+            return
+        trusted = self._fcc.get_trusted(master_id)
+        for trusted_id in trusted:
+            application_name = self._fcc.get_application_name(trusted_id)
+            master = None
+            if application_name == 'MASTER':
+                master = trusted_id
+                self._fcc.set_feed_ids_radius(master, step)
+            elif application_name == prev_app_name or prev_app_name == 'MASTER':
+                master = self._fcc.get_master_id_from_feed(trusted_id)
+                self._fcc.set_feed_ids_radius(master, step)
+            else:
+                return
+            self.__check_trusted(master, step + 1, radius, application_name)
