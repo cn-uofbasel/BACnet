08d3a76c853c0d5163680c380a58ac4a7590769b
Moritz <moritz.wuerth@stud.unibas.ch>
2020-05-24 17:44:21 +0200

    change of interface tests for master feed

--- groups/07-14-logCtrl/src/tests/test_chat_connection.py
+++ groups/07-14-logCtrl/src/tests/test_chat_connection.py
@@ -1,46 +1,49 @@
-import hashlib  # Comes with python
-import secrets  # Comes with python
-from nacl.signing import SigningKey
 from testfixtures import LogCapture
 import os
-from ..logStore.funcs.event import Content, Event, Meta
 from ..logStore.appconn.chat_connection import ChatFunction
+from ..logStore.funcs.EventCreationTool import EventFactory
 
 
 def test_get_chat_event():
     try:
         with LogCapture() as log_cap:
-            private_key = secrets.token_bytes(32)
-            signing_key = SigningKey(private_key)
-            public_key_feed_id = signing_key.verify_key.encode()
+            ecf = EventFactory()
 
-            content0 = Content('chat/whateveraction',
-                               {'messagekey': 'hallo zusammen', 'chat_id': '1', 'timestampkey': 10})
-            hash_of_content = hashlib.sha256(content0.get_as_cbor()).hexdigest()
-            hash_of_prev = None
-            meta = Meta(public_key_feed_id, 0, hash_of_prev, 'ed25519', ('sha256', hash_of_content))
-            signature = signing_key.sign(meta.get_as_cbor())._signature
-            event = Event(meta, signature, content0).get_as_cbor()
+            cf = ChatFunction()
 
-            connector = ChatFunction()
-            connector.insert_chat_msg(event)
-            meta = Meta(public_key_feed_id, 1, hash_of_prev, 'ed25519', ('sha256', hash_of_content))
-            content1 = Content('chat/whateveraction',
-                               {'messagekey': 'wie gehts?', 'chat_id': '1', 'timestampkey': 20})
-            signature = signing_key.sign(meta.get_as_cbor())._signature
-            event = Event(meta, signature, content1).get_as_cbor()
-            connector.insert_chat_msg(event)
-            content2 = Content('chat/whateveraction',
-                               {'messagekey': 'schönes Wetter heute', 'chat_id': '1', 'timestampkey': 30})
-            meta = Meta(public_key_feed_id, 2, hash_of_prev, 'ed25519', ('sha256', hash_of_content))
-            signature = signing_key.sign(meta.get_as_cbor())._signature
-            event = Event(meta, signature, content2).get_as_cbor()
-            connector.insert_chat_msg(event)
+            new_event = ecf.next_event('MASTER/MASTER', {})
+            cf.insert_event(new_event)
+
+            feed = EventFactory()
+            feed2 = EventFactory()
+            feed3 = EventFactory()
+
+            new_event = feed.next_event('chat/MASTER', {'master_feed': ecf.get_feed_id()})
+            cf.insert_event(new_event)
+
+            new_event = feed2.next_event('chat/MASTER', {'master_feed': ecf.get_feed_id()})
+            cf.insert_event(new_event)
+
+            new_event = feed3.next_event('chat/MASTER', {'master_feed': ecf.get_feed_id()})
+            cf.insert_event(new_event)
+
+            new_event = feed.next_event('chat/whateveraction',
+                                        {'messagekey': 'hallo zusammen', 'chat_id': '1', 'timestampkey': 10})
+            cf.insert_event(new_event)
+
+            new_event = feed.next_event('chat/whateveraction',
+                                        {'messagekey': 'wie gehts?', 'chat_id': '1', 'timestampkey': 20})
+            cf.insert_event(new_event)
+
+            new_event = feed.next_event('chat/whateveraction',
+                                        {'messagekey': 'schönes Wetter heute', 'chat_id': '1', 'timestampkey': 30})
+            cf.insert_event(new_event)
+
             print(log_cap)
-            q = connector.get_chat_since(15, '1')
+            q = cf.get_chat_since(15, '1')
             assert q[0][0] == 'wie gehts?'
             assert q[1][0] == 'schönes Wetter heute'
-            t = connector.get_full_chat('1')
+            t = cf.get_full_chat('1')
             assert t[0][0] == 'hallo zusammen'
             assert t[1][0] == 'wie gehts?'
             assert t[2][0] == 'schönes Wetter heute'
