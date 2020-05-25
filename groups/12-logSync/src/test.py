4aaf22d420e4f1bb1625728a876fe0e1dab18802
Carlos Tejera <carlos.tejer@stud.unibas.ch>
2020-05-25 22:23:25 +0200

    Auto stash before merge of "group12" and "origin/group12"

---
+++ groups/12-logSync/src/test.py
@@ -0,0 +1,26 @@
+from logStore.transconn.database_connector import  DatabaseConnector
+import cbor2
+
+
+dc = DatabaseConnector()
+
+feed_ids = dc.get_all_feed_ids()
+print(feed_ids)
+n = dc.get_current_seq_no(feed_ids[1])
+print("seq num:", n, "(feed 1)")
+tmp = 0
+for i in range(0, n + 1):
+    l = cbor2.loads(dc.get_event(feed_ids[1], i))
+    tmp = cbor2.dumps(l)
+    print(cbor2.loads(l[0])) # content bits
+    print(cbor2.loads(l[1]))
+    print(cbor2.loads(l[2])) # event
+    print("-------------------")
+
+print("************************************")
+for id in feed_ids:
+    print(id)
+    item = cbor2.loads(dc.get_current_event(id))
+    for i in item:
+        print(i)
+    print("----------------------------------")
