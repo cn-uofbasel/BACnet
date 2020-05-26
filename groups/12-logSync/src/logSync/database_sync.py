ed717c7216d4663d7ec99b61a9fe5d1afa0f21ca
Carlos Tejera <carlos.tejer@stud.unibas.ch>
2020-05-26 18:21:08 +0200

    Add validation of received events

--- groups/12-logSync/src/logSync/database_sync.py
+++ groups/12-logSync/src/logSync/database_sync.py
@@ -1,5 +1,10 @@
 from logStore.transconn.database_connector import DatabaseConnector
 import cbor2
+import hashlib
+
+
+def get_hash(blob):
+    return hashlib.sha256(blob).digest()
 
 
 def compare_feeds(list_of_feeds):
@@ -46,15 +51,69 @@ def filter_events(list_with_needed_extensions):
     return event_list
 
 
-def sync_database(i_want_extensions_list, feed_extensions):
-    feed_extensions = cbor2.loads(feed_extensions)
+def sync_database(i_want_extensions_list, received_extensions):
+    received_extensions = cbor2.loads(received_extensions)
     dc = DatabaseConnector()
-    if len(i_want_extensions_list) != len(feed_extensions):
+    if len(i_want_extensions_list) != len(received_extensions):
         print("Something went wrong..")
         return
 
     for i, val in enumerate(i_want_extensions_list):
-        appended_events_list = feed_extensions[i]
-        for ev in appended_events_list:
-            dc.add_event(ev)
+        appended_events_list = received_extensions[i]
+        # Check if valid
+        if verify_validation(val, appended_events_list[0]):
+            for ev in appended_events_list:
+                dc.add_event(ev)
+        else:
+            print("The extension is not valid! Sync not possible.")
+            return
+
     print("Finished synchronising!")
+
+
+def verify_validation(i_want_list, received_event):
+    feed_id = i_want_list[0]
+    seq_num = i_want_list[1]
+
+    dc = DatabaseConnector()
+    last_event = dc.get_event(feed_id, seq_num)
+
+    # Controlling if last event exists
+    if last_event is None:
+        # If the list has -1, it means it is a new feed to create
+        if seq_num == -1:
+            print("Awaiting creation of new feeds:", feed_id)
+            return True
+        else:
+            return False
+
+    last_event = cbor2.loads(last_event)
+    last_meta = cbor2.loads(last_event[0])  # meta
+
+    received_event = cbor2.loads(received_event)
+    received_meta = cbor2.loads(received_event[0])  # meta
+
+    # Controlling feed ids
+    if last_meta[0] != received_meta[0]:
+        return False
+    print(last_meta[0])
+    print(received_meta[0])
+
+    # Controlling sequence numbers
+    if last_meta[1] != received_meta[1]:
+        return False
+    print(last_meta[1])
+    print(received_meta[1])
+
+    # Controlling last meta hash value / prev hash value
+    if received_meta[2][0] != 0 or get_hash(last_event[0]) != received_meta[2][1]:
+        return False
+    print(get_hash(last_event[0]))
+    print(received_meta[2][1])
+
+    if last_meta[3] != received_meta[3]:
+        return False
+    print(last_meta[3])
+    print(received_meta[3])
+
+    return True
--- groups/12-logSync/src/test.py
+++ groups/12-logSync/src/test.py
@@ -1,11 +1,12 @@
 from logStore.transconn.database_connector import  DatabaseConnector
 import cbor2
+import event
+
 
 
 dc = DatabaseConnector()
 
 feed_ids = dc.get_all_feed_ids()
-print(feed_ids)
 n = dc.get_current_seq_no(feed_ids[1])
 print("seq num:", n, "(feed 1)")
 tmp = 0
@@ -13,14 +14,8 @@ for i in range(0, n + 1):
     l = cbor2.loads(dc.get_event(feed_ids[1], i))
     tmp = cbor2.dumps(l)
     print(cbor2.loads(l[0])) # content bits
-    print(cbor2.loads(l[1]))
+    # print(l[1])
     print(cbor2.loads(l[2])) # event
+    print(event.get_hash(l[0]))
     print("-------------------")
 
-print("************************************")
-for id in feed_ids:
-    print(id)
-    item = cbor2.loads(dc.get_current_event(id))
-    for i in item:
-        print(i)
-    print("----------------------------------")
