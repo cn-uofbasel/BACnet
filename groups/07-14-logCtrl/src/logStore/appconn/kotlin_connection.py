d10acbdc652d3bdf0b930fbdc2e95bb471e695a9
Moritz <moritz.wuerth@stud.unibas.ch>
2020-05-28 12:34:24 +0200

    add comments at the kotlin and chat interface

--- groups/07-14-logCtrl/src/logStore/appconn/kotlin_connection.py
+++ groups/07-14-logCtrl/src/logStore/appconn/kotlin_connection.py
@@ -10,29 +10,36 @@ class KotlinFunction(Function):
     def insert_data(self, cbor):
         """adds a new chat element as cbor
 
-                Tested and works!"""
+        @:parameter event: The new cbor event to be added
+        @:returns 1 if successful, -1 if any error occurred
+        """
         self.insert_event(cbor)
 
     def get_usernames_and_feed_id(self):
-        """returns all current usernames with the corresponding feed id
+        """Get all current usernames with the corresponding feed id
 
-                Tested and works!"""
+        @:returns a list with all Kotlin usernames and the corresponding feed id
+        """
         return self._handler.get_usernames_and_feed_id()
 
     def get_all_entries_by_feed_id(self, feed_id):
-        """returns all elements with the corresponding feed id, thus all events of a user
+        """Get all elements with the corresponding feed id, thus all events of a user
 
-                Tested and works!"""
+        @:parameter feed_id: the feed id of a user
+        @:returns a list of all Kotlin entries with the correct feed id
+        """
         return self._handler.get_all_entries_by_feed_id(feed_id)
 
     def get_all_kotlin_events(self):
-        """returns all existing kotlin elements that are in the database
+        """Get all existing kotlin elements that are in the database
 
-                Tested and works!"""
+        @:returns a list of all Kotlin entries
+        """
         return self._handler.get_all_kotlin_events()
 
     def get_last_kotlin_event(self):
-        """returns only the last added kotlin element
+        """Get only the last added kotlin element
 
-                Tested and works!"""
+        @:returns a only the last Kotlin entry as cbor
+        """
         return self._handler.get_last_kotlin_event()
