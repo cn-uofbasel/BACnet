85503ed70abc7f14719b2784d978c3e64b600b9e
Gunes Aydin <guenes.aydin@unibas.ch>
2020-12-20 13:49:38 +0100

    Add skeleton type wrappers

---
+++ redez-sem-hs20/groups/07-decentTCP/src/BackEndWrapper.py
@@ -0,0 +1,10 @@
+import SocketWrapper
+
+class BackEndWrapper:
+
+    def __init__(self, lulw, omeganay, hidin):
+        self.SocketWrapper(lulw, omeganay, hidin)
+    
+    def send(sendStr) -> None:
+        self.SocketWrapper.send(sendStr)
+
