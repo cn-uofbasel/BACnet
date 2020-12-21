2c5399d592278a0ede633bef4c51a1c20ff87413
DESKTOP-TEL42RA\Renato <renato.farruggio@bluewin.ch>
2020-12-21 12:43:11 +0100

    Add double ratchet demo code.

---
+++ redez-sem-hs20/groups/03-doubleRatchet/nonRedezDoubleRatchetDemo/double_rathet_cipher_v2.py
@@ -0,0 +1,146 @@
+# All of the following is copied from:
+# https://nfil.dev/coding/encryption/python/double-ratchet-example/
+
+### I USED 'pip install pycryptodome', because pycrypto didn't work to install.
+### This is a fork, so it might have security bugs.
+### Got the tip from https://stackoverflow.com/a/54142469
+
+
+# Requirements:
+# apt install python3 python3-pip
+# pip3 install cryptography==2.8 pycrypto
+
+import base64
+
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.ed25519 import \
+        Ed25519PublicKey, Ed25519PrivateKey
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+
+from Crypto.Cipher import AES
+
+def b64(msg):
+    # base64 encoding helper function
+    return base64.encodebytes(msg).decode('utf-8').strip()
+
+def hkdf(inp, length):
+    # use HKDF on an input to derive a key
+    hkdf = HKDF(algorithm=hashes.SHA256(), length=length, salt=b'',
+                info=b'', backend=default_backend())
+    return hkdf.derive(inp)
+
+class SymmRatchet(object):
+    def __init__(self, key):
+        self.state = key
+
+    def next(self, inp=b''):
+        # turn the ratchet, changing the state and yielding a new key and IV
+        output = hkdf(self.state + inp, 80)
+        self.state = output[:32]
+        outkey, iv = output[32:64], output[64:]
+        return outkey, iv
+
+class Bob(object):
+    def __init__(self):
+        # generate Bob's keys
+        self.IKb = X25519PrivateKey.generate()
+        self.SPKb = X25519PrivateKey.generate()
+        self.OPKb = X25519PrivateKey.generate()
+
+    def x3dh(self, alice):
+        # perform the 4 Diffie Hellman exchanges (X3DH)
+        dh1 = self.SPKb.exchange(alice.IKa.public_key())
+        dh2 = self.IKb.exchange(alice.EKa.public_key())
+        dh3 = self.SPKb.exchange(alice.EKa.public_key())
+        dh4 = self.OPKb.exchange(alice.EKa.public_key())
+        # the shared key is KDF(DH1||DH2||DH3||DH4)
+        self.sk = hkdf(dh1 + dh2 + dh3 + dh4, 32)
+        print('[Bob]\tShared key:', b64(self.sk))
+
+    def init_ratchets(self):
+        # initialise the root chain with the shared key
+        self.root_ratchet = SymmRatchet(self.sk)
+        # initialise the sending and recving chains
+        self.recv_ratchet = SymmRatchet(self.root_ratchet.next()[0])
+        self.send_ratchet = SymmRatchet(self.root_ratchet.next()[0])
+        # initialise Bob's DH ratchet
+        self.DHratchet = X25519PrivateKey.generate()
+
+    def dh_ratchet(self, alice_public):
+        # perform a DH ratchet rotation using Alice's public key
+        dh_recv = self.DHratchet.exchange(alice_public)
+        shared_recv = self.root_ratchet.next(dh_recv)[0]
+        # use Alice's public and our old private key
+        # to get a new recv ratchet
+        self.recv_ratchet = SymmRatchet(shared_recv)
+        print('[Bob]\tRecv ratchet seed:', b64(shared_recv))
+        # generate a new key pair and send ratchet
+        # our new public key will be sent with the next message to Alice
+        self.DHratchet = X25519PrivateKey.generate()
+        dh_send = self.DHratchet.exchange(alice_public)
+        shared_send = self.root_ratchet.next(dh_send)[0]
+        self.send_ratchet = SymmRatchet(shared_send)
+        print('[Bob]\tSend ratchet seed:', b64(shared_send))
+
+
+class Alice(object):
+    def __init__(self):
+        # generate Alice's keys
+        self.IKa = X25519PrivateKey.generate()
+        self.EKa = X25519PrivateKey.generate()
+
+    def x3dh(self, bob):
+        # perform the 4 Diffie Hellman exchanges (X3DH)
+        dh1 = self.IKa.exchange(bob.SPKb.public_key())
+        dh2 = self.EKa.exchange(bob.IKb.public_key())
+        dh3 = self.EKa.exchange(bob.SPKb.public_key())
+        dh4 = self.EKa.exchange(bob.OPKb.public_key())
+        # the shared key is KDF(DH1||DH2||DH3||DH4)
+        self.sk = hkdf(dh1 + dh2 + dh3 + dh4, 32)
+        print('[Alice]\tShared key:', b64(self.sk))
+
+    def init_ratchets(self):
+        # initialise the root chain with the shared key
+        self.root_ratchet = SymmRatchet(self.sk)
+        # initialise the sending and recving chains
+        self.send_ratchet = SymmRatchet(self.root_ratchet.next()[0])
+        self.recv_ratchet = SymmRatchet(self.root_ratchet.next()[0])
+        # Alice's DH ratchet starts out uninitialised
+        self.DHratchet = None
+
+    def dh_ratchet(self, bob_public):
+        # perform a DH ratchet rotation using Bob's public key
+        if self.DHratchet is not None:
+            # the first time we don't have a DH ratchet yet
+            dh_recv = self.DHratchet.exchange(bob_public)
+            shared_recv = self.root_ratchet.next(dh_recv)[0]
+            # use Bob's public and our old private key
+            # to get a new recv ratchet
+            self.recv_ratchet = SymmRatchet(shared_recv)
+            print('[Alice]\tRecv ratchet seed:', b64(shared_recv))
+        # generate a new key pair and send ratchet
+        # our new public key will be sent with the next message to Bob
+        self.DHratchet = X25519PrivateKey.generate()
+        dh_send = self.DHratchet.exchange(bob_public)
+        shared_send = self.root_ratchet.next(dh_send)[0]
+        self.send_ratchet = SymmRatchet(shared_send)
+        print('[Alice]\tSend ratchet seed:', b64(shared_send))
+
+
+alice = Alice()
+bob = Bob()
+
+# Alice performs an X3DH while Bob is offline, using his uploaded keys
+alice.x3dh(bob)
+
+# Bob comes online and performs an X3DH using Alice's public keys
+bob.x3dh(alice)
+
+# Initialize their symmetric ratchets
+alice.init_ratchets()
+bob.init_ratchets()
+
+# Initialise Alice's sending ratchet with Bob's public key
+alice.dh_ratchet(bob.DHratchet.public_key())
