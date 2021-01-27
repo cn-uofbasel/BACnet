b9dcbde496544d2dd1b0a22b768173f90fd11b9f
DESKTOP-TEL42RA\Renato <renato.farruggio@bluewin.ch>
2021-01-27 14:40:34 +0100

    Add serializability of keys, and saving/loading keys from savefiles.

--- redez-sem-hs20/groups/03-doubleRatchet/nonRedezDoubleRatchetDemo/local_double_ratchet.py
+++ redez-sem-hs20/groups/03-doubleRatchet/nonRedezDoubleRatchetDemo/local_double_ratchet.py
@@ -13,13 +13,17 @@
 import base64
 
 from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
 from cryptography.hazmat.primitives.asymmetric.ed25519 import \
         Ed25519PublicKey, Ed25519PrivateKey
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.backends import default_backend
 
 from Crypto.Cipher import AES
+
+import os
+
+from Crypto.Hash import SHA256
 
 def b64(msg):
     # base64 encoding helper function
@@ -40,6 +44,17 @@ def unpad(msg):
     # remove pkcs7 padding
     return msg[:-msg[-1]]
 
+def serialize_public_key(public_key: X25519PublicKey) -> bytes:
+    return public_key.public_bytes(encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw)
+def deserialize_public_key(public_bytes) -> X25519PublicKey:
+    return X25519PublicKey.from_public_bytes(public_bytes)
+
+def serialize_private_key(private_key: X25519PrivateKey) -> bytes:
+    return private_key.private_bytes(encoding=serialization.Encoding.Raw, format=serialization.PrivateFormat.Raw,
+                                     encryption_algorithm=serialization.NoEncryption())
+def deserialize_private_key(private_bytes) -> X25519PrivateKey:
+    return X25519PrivateKey.from_private_bytes(private_bytes)
+
 class SymmRatchet(object):
     def __init__(self, key):
         self.state = key
@@ -51,12 +66,83 @@ class SymmRatchet(object):
         outkey, iv = output[32:64], output[64:]
         return outkey, iv
 
+#######################################################################
+path_keys_alice = os.getcwd() + '/keys_alice.txt'
+def load_alice_keys() -> (X25519PrivateKey, X25519PrivateKey):
+    # If existing, load the saved keys for alice.
+    # If they do not already exists, generate new keys and save them.
+    # Returns 2 keys:
+    # IKa: X25519PrivateKey
+    # EKa: X25519PrivateKey
+    try:
+        with open(path_keys_alice, 'rb') as f:
+            lines = f.read()
+            assert(len(lines) == 64)
+            IKa_bytes = lines[:32]
+            EKa_bytes = lines[32:]
+            IKa = deserialize_private_key(IKa_bytes)
+            EKa = deserialize_private_key(EKa_bytes)
+            print("Loaded saved keys.")
+    except FileNotFoundError:
+        print("No keys found. Creating new keys...")
+        IKa = X25519PrivateKey.generate()
+        EKa = X25519PrivateKey.generate()
+        with open(path_keys_alice, 'wb') as f:
+            for key in [IKa, EKa]:
+                f.write(serialize_private_key(key))
+            print("Keys saved.")
+        pass
+    return (IKa, EKa)
+
+path_keys_bob = os.getcwd() + '/keys_bob.txt'
+def load_bob_keys():
+    # If existing, load the saved keys for bob.
+    # If they do not already exists, generate new keys and save them.
+    # Returns 3 keys:
+    # IKb: X25519PrivateKey
+    # SPKb: X25519PrivateKey
+    # OPKb: X25519PrivateKey
+    try:
+        with open(path_keys_bob, 'rb') as f:
+            lines = f.read()
+            assert(len(lines) == 96)
+            IKb_bytes = lines[:32]
+            SPKb_bytes = lines[32:64]
+            OPKb_bytes = lines[64:]
+            IKb = deserialize_private_key(IKb_bytes)
+            SPKb = deserialize_private_key(SPKb_bytes)
+            OPKb = deserialize_private_key(OPKb_bytes)
+            print("Loaded saved keys.")
+    except FileNotFoundError:
+        print("No keys found. Creating new keys...")
+        IKb = X25519PrivateKey.generate()
+        SPKb = X25519PrivateKey.generate()
+        OPKb = X25519PrivateKey.generate()
+        with open(path_keys_bob, 'wb') as f:
+            for key in [IKb, SPKb, OPKb]:
+                f.write(serialize_private_key(key))
+            print("Keys saved.")
+        pass
+    return (IKb, SPKb, OPKb)
+
+'''
+    
+    self.IKb.private_bytes(encoding=serialization.Encoding.Raw, format=serialization.PrivateFormat.Raw,
+                           encryption_algorithm=serialization.NoEncryption())
+    private_bytes = b'\x90\xc9\xdc\xa4\x99O\x9c\xfd&\x90\x8d\xce+\xb5\xf7_\xa6\x93\xeds\xe0\xc7/\xdc\x9eG%\xcf\xf3\xfa\x84q'
+    IKb_copy = X25519PrivateKey.from_private_bytes(private_bytes)
+    IKb_copy.private_bytes(encoding=serialization.Encoding.Raw, format=serialization.PrivateFormat.Raw,
+                           encryption_algorithm=serialization.NoEncryption())
+    load_bob_keys()
+
+'''
+#######################################################################
+
 class Bob(object):
     def __init__(self):
         # generate Bob's keys
-        self.IKb = X25519PrivateKey.generate()
-        self.SPKb = X25519PrivateKey.generate()
-        self.OPKb = X25519PrivateKey.generate()
+        (self.IKb, self.SPKb, self.OPKb) = load_bob_keys()
+
 
     def x3dh(self, alice):
         # perform the 4 Diffie Hellman exchanges (X3DH)
@@ -108,12 +194,10 @@ class Bob(object):
         msg = unpad(AES.new(key, AES.MODE_CBC, iv).decrypt(cipher))
         print('[Bob]\tDecrypted message:', msg)
 
-
 class Alice(object):
     def __init__(self):
         # generate Alice's keys
-        self.IKa = X25519PrivateKey.generate()
-        self.EKa = X25519PrivateKey.generate()
+        (self.IKa, self.EKa) = load_alice_keys()
 
     def x3dh(self, bob):
         # perform the 4 Diffie Hellman exchanges (X3DH)
