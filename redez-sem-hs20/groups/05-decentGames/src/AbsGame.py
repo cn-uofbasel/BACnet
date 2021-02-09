f3752c5e90b2674c7f86b196ecbf134d6b538b84
Carlos Andrés Tejera <32110668+carlostejera@users.noreply.github.com>
2021-02-09 15:44:12 +0100

    Add abstract methods for other games

---
+++ redez-sem-hs20/groups/05-decentGames/src/AbsGame.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+
+class AbsGame(ABC):
+
+    @abstractmethod
+    def get_board(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_turn_of(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_who_am_i(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_allowed_moves(self):
+        pass
+
+    @abstractmethod
+    def get_dic(self):
+        pass
+
+    @abstractmethod
+    def get_playable(self):
+        pass
+
+    @abstractmethod
+    def set_playable(self, state: bool):
+        pass
+
+    @abstractmethod
+    def move(self, move: str):
+        pass
+
+    @abstractmethod
+    def get_ginfo(self):
+        pass
+
