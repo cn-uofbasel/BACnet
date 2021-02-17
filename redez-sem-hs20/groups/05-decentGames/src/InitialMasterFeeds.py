2aa068e9820474d07dd2afd90ddd1ebd573ac499
Alexander Oxley <alexander.oxley@stud.unibas.ch>
2021-02-17 23:41:58 +0100

    needed to sync master feeds initially

---
+++ redez-sem-hs20/groups/05-decentGames/src/InitialMasterFeeds.py
@@ -0,0 +1,20 @@
+import udp_connection
+import argparse
+from logSync import database_sync
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tool for synchronisation')
+
+    parser.add_argument('--sync', metavar='DIR', nargs=2)
+    parser.add_argument('--dump', metavar='DIR')
+    parser.add_argument('--server', metavar='server', nargs=1)
+    parser.add_argument('--client', metavar='client', nargs=1)
+    args = parser.parse_args()
+
+    if args.server is not None:
+        server = udp_connection.Server(args.server[0])
+
+    if args.client is not None:
+        client = udp_connection.Client(args.client[0])
+        database_sync.sync_database(client.get_list_of_needed_extensions(), client.get_packet_to_receive_as_bytes())
