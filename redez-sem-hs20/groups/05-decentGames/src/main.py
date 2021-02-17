5cc32f36a6d18d73030d0695df4f987d693f8522
Carlos Andrés Tejera <32110668+carlostejera@users.noreply.github.com>
2021-02-17 13:28:20 +0100

    Update main.py

--- redez-sem-hs20/groups/05-decentGames/src/main.py
+++ redez-sem-hs20/groups/05-decentGames/src/main.py
@@ -10,21 +10,23 @@ import xmlrpc.client as rpc
 import _thread as t
 
 
-
 def initialise_server():
     RequestServer()
+
 
 if __name__ == '__main__':
     print('Hello, stranger!')
     while True:
-        inp = input('Choose what to do:\n')
-        args = inp.split(' ')
-        command = args[0]
+        inp = input('Choose what to do: ')
+        args = sys.argv
+        inputs = inp.split(' ')
+        command = inputs[0] if len(inputs) >= 1 else None
+        arg = inputs[1] if len(inputs) == 2 else None
         game = args[1] if len(args) >= 2 else None
-        arg = args[2] if len(args) >= 3 else None
-        ip1 = args[3] if len(args) >= 4 else None
-        ip2 = args[4] if len(args) == 5 else None
+        ip1 = args[2] if len(args) >= 3 else None
+        ip2 = args[3] if len(args) == 4 else None
         print(game, arg)
+        print(ip1, ip2)
         if command == '/play':
             if os.path.isfile('games/%s.dga' % arg) or os.path.isfile('games/%s.chess' % arg):
                 print('in')
