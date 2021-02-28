f73c670f4a6641d8550be2df7d59f9ccb775cfa2
Renato Farruggio <renato.farruggio@bluewin.ch>
2021-02-28 23:37:24 +0100

    Add a script to reset all conversations.

---
+++ redez-sem-hs20/groups/03-doubleRatchet/nonRedezDoubleRatchetDemo/reset.py
@@ -0,0 +1,43 @@
+from helpers import FOLDERNAME_KEYS
+import os
+
+# Running this will reset and delete all conversations.
+
+# TODO: ask user if he is sure.
+'''
+input = input("Are you sure? [y/n]")
+if input == 'y':
+    pass
+else:
+    print("Abort Deletion.")
+    exit()
+'''
+
+foldername = FOLDERNAME_KEYS.lstrip("/") + "/"
+
+try:
+    list = [item for item in os.listdir(os.getcwd() + FOLDERNAME_KEYS)]
+    for item in list:
+        os.remove(foldername + item)
+    os.removedirs(foldername)
+except FileNotFoundError:
+    pass
+
+
+
+foldername = "public_key/"
+
+try:
+    list = [item for item in os.listdir(os.getcwd() + "/public_key")]
+    for item in list:
+        os.remove(foldername + item)
+    os.removedirs(foldername)
+except FileNotFoundError:
+    pass
+
+try:
+    os.remove("cborDatabase.sqlite")
+except FileNotFoundError:
+    pass
+
+print("Deleted all conversations.")
