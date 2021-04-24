73764e6762fd58a7cbde86357d1bc2ab991e2770
Pascal Kunz <pasca.kunz@stud.unibas.ch>
2021-04-24 08:25:18 +0200

    create a seperate model for nodes

--- 21-fs-ias-lec/FrontEnd/socialgraph/models.py
+++ 21-fs-ias-lec/FrontEnd/socialgraph/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Nodes(models.Model):
+
+    def create(self):
+        nodes = [
+            {
+                "id": 1,
+                "name": "A",
+                "gender": "male"
+            },
+            {
+                "id": 2,
+                "name": "B",
+                "gender": "female"
+            },
+            {
+                "id": 3,
+                "name": "C",
+                "gender": "male"
+            },
+            {
+                "id": 4,
+                "name": "D",
+                "gender": "male"
+            },
+            {
+                "id": 5,
+                "name": "E",
+                "gender": "male"
+            },
+            {
+                "id": 6,
+                "name": "F",
+                "gender": "female"
+            },
+            {
+                "id": 7,
+                "name": "G",
+                "gender": "female"
+            },
+            {
+                "id": 8,
+                "name": "H",
+                "gender": "male"
+            },
+            {
+                "id": 9,
+                "name": "I",
+                "gender": "female"
+            },
+            {
+                "id": 10,
+                "name": "J",
+                "gender": "male"
+            },
+            {
+                "id": 11,
+                "name": "K",
+                "gender": "male"
+            },
+            {
+                "id": 12,
+                "name": "L",
+                "gender": "female"
+            },
+            {
+                "id": 13,
+                "name": "M",
+                "gender": "male"
+            },
+            {
+                "id": 14,
+                "name": "N",
+                "gender": "female"
+            },
+            {
+                "id": 15,
+                "name": "O",
+                "gender": "female"
+            },
+            {
+                "id": 16,
+                "name": "P",
+                "gender": "female"
+            }
+        ]
+        return nodes
--- 21-fs-ias-lec/FrontEnd/socialgraph/views.py
+++ 21-fs-ias-lec/FrontEnd/socialgraph/views.py
@@ -1,89 +1,9 @@
 from django.shortcuts import render
+from .models import Nodes
 
 # Create your views here.
 
-nodes = [
-    {
-      "id": 1,
-      "name": "A",
-      "gender": "male"
-    },
-    {
-      "id": 2,
-      "name": "B",
-      "gender": "female"
-    },
-    {
-      "id": 3,
-      "name": "C",
-      "gender": "male"
-    },
-    {
-      "id": 4,
-      "name": "D",
-      "gender": "male"
-    },
-    {
-      "id": 5,
-      "name": "E",
-      "gender": "male"
-    },
-    {
-      "id": 6,
-      "name": "F",
-      "gender": "female"
-    },
-    {
-      "id": 7,
-      "name": "G",
-      "gender": "female"
-    },
-    {
-      "id": 8,
-      "name": "H",
-      "gender": "male"
-    },
-    {
-      "id": 9,
-      "name": "I",
-      "gender": "female"
-    },
-    {
-      "id": 10,
-      "name": "J",
-      "gender": "male"
-    },
-    {
-      "id": 11,
-      "name": "K",
-      "gender": "male"
-    },
-    {
-      "id": 12,
-      "name": "L",
-      "gender": "female"
-    },
-    {
-      "id": 13,
-      "name": "M",
-      "gender": "male"
-    },
-    {
-      "id": 14,
-      "name": "N",
-      "gender": "female"
-    },
-    {
-      "id": 15,
-      "name": "O",
-      "gender": "female"
-    },
-    {
-      "id": 16,
-      "name": "P",
-      "gender": "female"
-    }
-]
+
 
 
 links = [
@@ -158,8 +78,10 @@ def home(request):
     return render(request, 'socialgraph/home.html', {'title': 'Home'})
 
 def users(request):
+    nodes = Nodes()
+
     context = {
-        'nodes': nodes,
+        'nodes': nodes.create(),
         'links': links,
         'testChart': 'static/socialgraph/testData.json'
     }
