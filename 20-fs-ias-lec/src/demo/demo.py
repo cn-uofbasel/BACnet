ec57b49bb418cd97dbfb64dc3ce3b79e497ec38e
fuubi <fuubi@protonmail.ch>
2021-04-26 17:25:40 +0200

    demo-script: print info and comment to console

--- 20-fs-ias-lec/src/demo/demo.py
+++ 20-fs-ias-lec/src/demo/demo.py
@@ -26,7 +26,7 @@ alice_digestmod = "sha256"
 alcie_h, alice_signer = None, None
 
 if not os.path.isfile("data/alice/alice-secret.key"):
-    # Alice did not create a key pair yet, lets create it!
+    print("Create Alice's key pair at data/alice/alice-secret.key") 
     alice_h = crypto.HMAC(alice_digestmod)
     alice_h.create()
     with open("data/alice/alice-secret.key", "w") as f: 
@@ -34,11 +34,14 @@ if not os.path.isfile("data/alice/alice-secret.key"):
         alice_signer = crypto.HMAC(alice_digestmod, alice_h.get_private_key())
 
 
+print("Read Alice's secret key.") 
 with open("data/alice/alice-secret.key", 'r') as f:
+        print("Create Bob's key pair at data/bob/bob-secret.key") 
         key = eval(f.read())
         alice_h = crypto.HMAC(alice_digestmod, key["private"], key["feed_id"])
         alice_signer = crypto.HMAC(alice_digestmod, bytes.fromhex(alice_h.get_private_key()))
 
+print("Create or load Alice's feed at data/alice/alice-feed.pcap") 
 alice_feed = feed.FEED(fname="data/alice/alice-feed.pcap", fid=alice_h.get_feed_id(), signer=alice_signer, create_if_notexisting=True, digestmod=alice_digestmod)
 
 ## Bob
@@ -46,29 +49,34 @@ bob_digestmod = "sha256"
 bob_h, bob_signer = None, None
 
 if not os.path.isfile("data/bob/bob-secret.key"):
-    # Bob did not create a key pair yet, lets create it!
+    print("Create Bob's key pair at data/bob/bob-secret.key") 
     bob_h = crypto.HMAC(bob_digestmod)
     bob_h.create()
     with open("data/bob/bob-secret.key", "w") as f:
         f.write('{\n  '+(',\n '.join(bob_h.as_string().split(','))[1:-1])+'\n}')
         
+print("Read Bob's secret key.") 
 with open("data/bob/bob-secret.key", 'r') as f:
         key = eval(f.read())
         bob_h = crypto.HMAC(bob_digestmod, key["private"], key["feed_id"])
         bob_signer = crypto.HMAC(bob_digestmod, bytes.fromhex(bob_h.get_private_key()))
 
 
+print("Create or load Boby's feed at data/bob/bob-feed.pcap") 
 bob_feed = feed.FEED(fname="data/bob/bob-feed.pcap", fid=bob_h.get_feed_id(), signer=bob_signer, create_if_notexisting=True, digestmod=bob_digestmod)
 
 
 
 import time
 
+print("Alice and Bob write to their log.") 
 alice_feed.write(["bacnet/chat", time.time(), "Hey, it is me Alice!"])
 bob_feed.write(["bacnet/chat",  time.time(), "Hey, it is me Bob!"])
 
 
-## We now pretend that Alice and Bob already synced their logs and therefore have access of the others log. 
+print("We now pretend that Alice and Bob already synced their logs and therefore have access of the others log.")
+
+print("\n Chat:\n") 
 chat = []
 for event in alice_feed: 
     if event.content()[0] == "bacnet/chat":
