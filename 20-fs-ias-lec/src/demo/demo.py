76ab882624352dfdfd06a90861c4db0b387c1d7e
fuubi <fuubi@protonmail.ch>
2021-04-26 11:50:50 +0200

    demo-script: init and write to a feed

--- 20-fs-ias-lec/src/demo/demo.py
+++ 20-fs-ias-lec/src/demo/demo.py
@@ -1,6 +1,17 @@
-from lib.crypto import HMAC
-import os.path
+import sys
+# add the lib to the module folder
+sys.path.append("./lib") 
 
+
+import os
+import crypto 
+import feed 
+
+
+
+## Dependencies: 
+# - [cbort2](https://pypi.org/project/cbor2/) 
+# Run pip3 install cbor2
 
 if not os.path.isdir("data"): 
     os.mkdir("data")
@@ -10,20 +21,48 @@ if not os.path.isdir("data"):
 # Generate a key pair 
 
 ## Alice
-digestmod = "sha256"
-h = HMAC(digestmod)
-h.create()
 
-f = open("data/alice/alice-secret.key", "w")
-f.write(h.as_string())
-f.close()
+alice_digestmod = "sha256"
+alcie_h, alice_signer = None, None
+
+if not os.path.isfile("data/alice/alice-secret.key"):
+    # Alice did not create a key pair yet, lets create it!
+    alice_h = crypto.HMAC(alice_digestmod)
+    alice_h.create()
+    with open("data/alice/alice-secret.key", "w") as f: 
+        f.write('{\n  '+(',\n '.join(alice_h.as_string().split(','))[1:-1])+'\n}')
+        alice_signer = crypto.HMAC(alice_digestmod, alice_h.get_private_key())
+
+else:
+    with open("data/alice/alice-secret.key", 'r') as f:
+            key = eval(f.read())
+            alice_h = crypto.HMAC(alice_digestmod, key["private"], key["feed_id"])
+            alice_signer = crypto.HMAC(alice_digestmod, bytes.fromhex(alice_h.get_private_key()))
+
+alice_feed = feed.FEED("data/alice/alice-feed.pcap", fid=alice_h.get_feed_id(), signer=alice_signer, create_if_notexisting=True, digestmod=alice_digestmod)
 
 ## Bob
-digestmod = "sha256"
-h = HMAC(digestmod)
-h.create()
-f = open("data/bob/bob-secret.key", "w")
-f.write(h.as_string())
-f.close()
+bob_digestmod = "sha256"
+bob_h, bob_signer = None, None
+
+if not os.path.isfile("data/bob/bob-secret.key"):
+    # Bob did not create a key pair yet, lets create it!
+    bob_h = crypto.HMAC(bob_digestmod)
+    bob_h.create()
+    with open("data/bob/bob-secret.key", "w") as f:
+        f.write('{\n  '+(',\n '.join(bob_h.as_string().split(','))[1:-1])+'\n}')
+        bob_signer = crypto.HMAC(bob_digestmod, bob_h.get_private_key())
+else:
+    with open("data/bob/bob-secret.key", 'r') as f:
+            key = eval(f.read())
+            bob_h = crypto.HMAC(bob_digestmod, key["private"], key["feed_id"])
+            bob_signer = crypto.HMAC(bob_digestmod, bytes.fromhex(bob_h.get_private_key()))
 
 
+bob_feed = feed.FEED("data/bob/bob-feed.pcap", fid=bob_h.get_feed_id(), signer=bob_signer, create_if_notexisting=True, digestmod=bob_digestmod)
+
+
+alice_feed.write(["bacnet/chat", "Hey, it is me Alice!"])
+bob_feed.write(["bacnet/chat", "Hey, it is me Bob!"])
+
+print(bob_feed.hprev)
