96e9ccf8dd8105e08e649ac98e3bf04ff8e14c93
yasminmeier <yasmin.meier@stud.unibas.ch>
2021-04-28 16:15:26 +0200

    generateFeed.py works now with cmd input for feed name

--- 21-fs-ias-lec/07-BackEnd/Feed/generateFeed.py
+++ 21-fs-ias-lec/07-BackEnd/Feed/generateFeed.py
@@ -7,27 +7,25 @@ import os
 import crypto
 import feed
 
-## Dependencies:
-# - [cbort2](https://pypi.org/project/cbor2/)
-# Run pip3 install cbor2
+name = input("please enter your name: ")
+print("your feed-name: " + name)
 
 if not os.path.isdir("data"):
     os.mkdir("data")
-    os.mkdir("data/alice")
-    os.mkdir("data/bob")
+
+if not os.path.isdir("data/" + name):
+    os.mkdir("data/" + name)
 
 # Generate a key pair
 
-## Alice
-
-name = sys.argv[1]
+## generate a feed
 digestmod = "sha256"
 h, signer = None, None
 
 # Generate a Key if non exists
 if not os.path.isfile("data/" + name + "/" + name + "-secret.key"):
     print("Create " + name + "'s key pair at data/" + name + "/" + name + "-secret.key")
-    h = crypto.HMAC(alice_digestmod)
+    h = crypto.HMAC(digestmod)
     h.create()
     with open("data/" + name + "/" + name + "-secret.key", "w") as f:
         f.write('{\n  ' + (',\n '.join(h.as_string().split(','))[1:-1]) + '\n}')
@@ -43,23 +41,5 @@ print("Create or load" + name + "'s feed at data/" + name + "/" + name + "-feed.
 myfeed = feed.FEED(fname="data/" + name + "/" + name + "-feed.pcap", fid=h.get_feed_id(), signer=signer,
                        create_if_notexisting=True, digestmod=digestmod)
 
-## Bob
-#bob_digestmod = "sha256"
-#bob_h, bob_signer = None, None
 
-#if not os.path.isfile("data/bob/bob-secret.key"):
-#    print("Create Bob's key pair at data/bob/bob-secret.key")
-#    bob_h = crypto.HMAC(bob_digestmod)
-#    bob_h.create()
-#    with open("data/bob/bob-secret.key", "w") as f:
-#        f.write('{\n  ' + (',\n '.join(bob_h.as_string().split(','))[1:-1]) + '\n}')
 
-#print("Read Bob's secret key.")
-#with open("data/bob/bob-secret.key", 'r') as f:
-#    key = eval(f.read())
-#    bob_h = crypto.HMAC(bob_digestmod, key["private"], key["feed_id"])
-#    bob_signer = crypto.HMAC(bob_digestmod, bytes.fromhex(bob_h.get_private_key()))
-
-#print("Create or load Boby's feed at data/bob/bob-feed.pcap")
-#bob_feed = feed.FEED(fname="data/bob/bob-feed.pcap", fid=bob_h.get_feed_id(), signer=bob_signer,
-#                     create_if_notexisting=True, digestmod=bob_digestmod)
