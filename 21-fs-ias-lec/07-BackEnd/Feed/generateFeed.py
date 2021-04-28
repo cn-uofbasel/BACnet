9a944429ffe61d7d48b6020d224385690d468a9e
Vera <vera.benz@stud.unibas.ch>
2021-04-28 15:27:33 +0200

    tried to change generateFeed to a geberic Feed

--- 21-fs-ias-lec/07-BackEnd/Feed/generateFeed.py
+++ 21-fs-ias-lec/07-BackEnd/Feed/generateFeed.py
@@ -1,7 +1,7 @@
 import sys
+
 # add the lib to the module folder
 sys.path.append("./lib")
-
 
 import os
 import crypto
@@ -21,47 +21,45 @@ if not os.path.isdir("data"):
 ## Alice
 
 name = sys.argv[1]
-alice_digestmod = "sha256"
-alice_h, alice_signer = None, None
+digestmod = "sha256"
+h, signer = None, None
 
 # Generate a Key if non exists
-if not os.path.isfile("data/alice/alice-secret.key"):
-    print("Create Alice's key pair at data/alice/alice-secret.key")
-    alice_h = crypto.HMAC(alice_digestmod)
-    alice_h.create()
-    with open("data/alice/alice-secret.key", "w") as f:
-        f.write('{\n  '+(',\n '.join(alice_h.as_string().split(','))[1:-1])+'\n}')
-        alice_signer = crypto.HMAC(alice_digestmod, alice_h.get_private_key())
+if not os.path.isfile("data/" + name + "/" + name + "-secret.key"):
+    print("Create " + name + "'s key pair at data/" + name + "/" + name + "-secret.key")
+    h = crypto.HMAC(alice_digestmod)
+    h.create()
+    with open("data/" + name + "/" + name + "-secret.key", "w") as f:
+        f.write('{\n  ' + (',\n '.join(h.as_string().split(','))[1:-1]) + '\n}')
+        signer = crypto.HMAC(digestmod, h.get_private_key())
 
-#
-print("Read Alice's secret key.")
-with open("data/alice/alice-secret.key", 'r') as f:
-        print("Create Bob's key pair at data/bob/bob-secret.key")
-        key = eval(f.read())
-        alice_h = crypto.HMAC(alice_digestmod, key["private"], key["feed_id"])
-        alice_signer = crypto.HMAC(alice_digestmod, bytes.fromhex(alice_h.get_private_key()))
+print("Read " + name + "'s secret key.")
+with open("data/" + name + "/" + name + "-secret.key", 'r') as f:
+    key = eval(f.read())
+    h = crypto.HMAC(digestmod, key["private"], key["feed_id"])
+    signer = crypto.HMAC(digestmod, bytes.fromhex(h.get_private_key()))
 
-print("Create or load Alice's feed at data/alice/alice-feed.pcap")
-alice_feed = feed.FEED(fname="data/alice/alice-feed.pcap", fid=alice_h.get_feed_id(), signer=alice_signer, create_if_notexisting=True, digestmod=alice_digestmod)
-
+print("Create or load" + name + "'s feed at data/" + name + "/" + name + "-feed.pcap")
+myfeed = feed.FEED(fname="data/" + name + "/" + name + "-feed.pcap", fid=h.get_feed_id(), signer=signer,
+                       create_if_notexisting=True, digestmod=digestmod)
 
 ## Bob
-bob_digestmod = "sha256"
-bob_h, bob_signer = None, None
+#bob_digestmod = "sha256"
+#bob_h, bob_signer = None, None
 
-if not os.path.isfile("data/bob/bob-secret.key"):
-    print("Create Bob's key pair at data/bob/bob-secret.key")
-    bob_h = crypto.HMAC(bob_digestmod)
-    bob_h.create()
-    with open("data/bob/bob-secret.key", "w") as f:
-        f.write('{\n  ' + (',\n '.join(bob_h.as_string().split(','))[1:-1]) + '\n}')
+#if not os.path.isfile("data/bob/bob-secret.key"):
+#    print("Create Bob's key pair at data/bob/bob-secret.key")
+#    bob_h = crypto.HMAC(bob_digestmod)
+#    bob_h.create()
+#    with open("data/bob/bob-secret.key", "w") as f:
+#        f.write('{\n  ' + (',\n '.join(bob_h.as_string().split(','))[1:-1]) + '\n}')
 
-print("Read Bob's secret key.")
-with open("data/bob/bob-secret.key", 'r') as f:
-    key = eval(f.read())
-    bob_h = crypto.HMAC(bob_digestmod, key["private"], key["feed_id"])
-    bob_signer = crypto.HMAC(bob_digestmod, bytes.fromhex(bob_h.get_private_key()))
+#print("Read Bob's secret key.")
+#with open("data/bob/bob-secret.key", 'r') as f:
+#    key = eval(f.read())
+#    bob_h = crypto.HMAC(bob_digestmod, key["private"], key["feed_id"])
+#    bob_signer = crypto.HMAC(bob_digestmod, bytes.fromhex(bob_h.get_private_key()))
 
-print("Create or load Boby's feed at data/bob/bob-feed.pcap")
-bob_feed = feed.FEED(fname="data/bob/bob-feed.pcap", fid=bob_h.get_feed_id(), signer=bob_signer,
-                     create_if_notexisting=True, digestmod=bob_digestmod)
+#print("Create or load Boby's feed at data/bob/bob-feed.pcap")
+#bob_feed = feed.FEED(fname="data/bob/bob-feed.pcap", fid=bob_h.get_feed_id(), signer=bob_signer,
+#                     create_if_notexisting=True, digestmod=bob_digestmod)
