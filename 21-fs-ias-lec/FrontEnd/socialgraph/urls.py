de0a166948eaf2a62efacfc0d2bcfc656046be71
Sebastian <sebastian.schlachter@stud.unibas.ch>
2021-05-03 11:37:10 +0200

    WIP on Profile Page
    Added Header with username and picture.
    Added Subcolumns for details.

--- 21-fs-ias-lec/FrontEnd/socialgraph/urls.py
+++ 21-fs-ias-lec/FrontEnd/socialgraph/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='socialgraph-home'),
     path('users/', views.users, name='socialgraph-users'),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('feed/', views.feed, name='socialgraph-feed'),
     path('profile/<pk>/', views.PostDetailView.as_view(), name='profile-detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
