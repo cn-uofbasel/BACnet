c37c34b0a11b74f9dd4822c61561fcb283a974da
emugdan <esther.mugdan@stud.unibas.ch>
2021-05-06 16:21:13 +0200

    person

--- 21-fs-ias-lec/07-BackEnd/generateFeeds.py
+++ 21-fs-ias-lec/07-BackEnd/generateFeeds.py
@@ -9,22 +9,27 @@ from Person import Person
 def main():
 
     # set a name to a feed
-    yasminFeed = Feed.Feed("yasmin")
+    yasmin = Feed.Feed("yasmin")
     esther = Feed.Feed("esther")
     vera = Feed.Feed("vera")
 
     # Feed erstellen
-    yasminFeed.generateOwnFeed()
+    yasmin.generateOwnFeed()
     esther.generateOwnFeed()
     vera.generateOwnFeed()
 
     # set person
-    yasminPerson = Person.Person(yasminFeed.id, yasminFeed.name, yasminFeed)
+    yasminPerson = Person.Person(yasmin.id, yasmin.name, yasmin)
     yasminPerson.follow(esther.id, esther.name)
     yasminPerson.follow(vera.id, vera.name)
     yasminPerson.printFollowList()
     yasminPerson.unfollow(vera.id)
     yasminPerson.printFollowList()
+
+    veraPerson = Person.Person(vera.id, vera.name, vera)
+    veraPerson.follow(esther.id, esther.name)
+    veraPerson.follow(yasmin.id, yasmin.name)
+    veraPerson.printFollowList()
 
 
 if __name__ == "__main__":
