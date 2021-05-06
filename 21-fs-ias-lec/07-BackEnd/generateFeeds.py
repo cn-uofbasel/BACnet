53e4a00329a1e38bc3d4665544274d52d8e4ccde
Vera <vera.benz@stud.unibas.ch>
2021-05-06 15:20:59 +0200

    in generateFeeds is now a main method

--- 21-fs-ias-lec/07-BackEnd/generateFeeds.py
+++ 21-fs-ias-lec/07-BackEnd/generateFeeds.py
@@ -1,35 +1,40 @@
 import sys
+
 sys.path.append("lib")
 
 from Feed import Feed
 from FollowList import FollowList
 
-# set a name to a feed
-yasmin = Feed.Feed("yasmin")
-esther = Feed.Feed("esther")
-vera = Feed.Feed("vera")
 
-# Feed erstellen
-yasmin.generateOwnFeed()
-esther.generateOwnFeed()
-vera.generateOwnFeed()
+def main():
+    # set a name to a feed
+    yasmin = Feed.Feed("yasmin")
+    esther = Feed.Feed("esther")
+    vera = Feed.Feed("vera")
 
-#Folgen in Feed eintragen
-yasmin.writeFollowToFeed("esther")
-esther.writeFollowToFeed("yasmin")
-yasmin.writeFollowToFeed("vera")
+    # Feed erstellen
+    yasmin.generateOwnFeed()
+    esther.generateOwnFeed()
+    vera.generateOwnFeed()
 
-#yasmin.readFollowFromFeed()
-#esther.readFollowFromFeed()
-#vera.readFollowFromFeed()
+    # Folgen in Feed eintragen
+    yasmin.writeFollowToFeed("esther", 1)
+    esther.writeFollowToFeed("yasmin", 2)
+    yasmin.writeFollowToFeed("vera", 3)
 
-#Followliste zum feed erstellen
-yasminsList = FollowList.FollowList(yasmin)
-esthersList = FollowList.FollowList(esther)
-verasList = FollowList.FollowList(vera)
+    # yasmin.readFollowFromFeed()
+    # esther.readFollowFromFeed()
+    # vera.readFollowFromFeed()
 
-# Die Listen auslesen und anzeigen
-yasminsList.getList()
-esthersList.getList()
-verasList.getList()
+    # Followliste zum feed erstellen
+    yasminsList = FollowList.FollowList(yasmin)
+    esthersList = FollowList.FollowList(esther)
+    verasList = FollowList.FollowList(vera)
 
+    # Die Listen auslesen und anzeigen
+    yasminsList.getList()
+    esthersList.getList()
+    verasList.getList()
+
+if __name__ == "__main__":
+    main()
