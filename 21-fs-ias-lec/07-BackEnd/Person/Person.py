9ab1283d64f4a2283f796eb3d959df2a96b6d78d
emugdan <esther.mugdan@stud.unibas.ch>
2021-05-06 13:59:34 +0200

    added person class

---
+++ 21-fs-ias-lec/07-BackEnd/Person/Person.py
@@ -0,0 +1,25 @@
+class Person:
+    name
+    id
+    feed
+    followlist
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __init__(self, id, name, feed):
+        self.id = id
+        self.name = name
+        self.feed = feed
+        self.followlist = dict()
+
+    def follow(self, id, name):
+        self.followlist[id] = Person(id, name)
+        # event in feed
+
+    def unfollow(self, id):
+        self.followlist.pop(id)
+
+    def getFollowList(self):
+        return self.followlist
