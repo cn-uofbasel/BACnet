0bee5683e686dcf9c40db9a065e5d321047557b8
pascalraphaelkunz <pasca.kunz@stud.unibas.ch>
2021-05-07 09:27:06 +0200

    add method create_Recommendations in importer.py

--- 21-fs-ias-lec/FrontEnd/socialgraph/importer.py
+++ 21-fs-ias-lec/FrontEnd/socialgraph/importer.py
@@ -1,6 +1,6 @@
 # Creates Profile Entries in the Database for each Node.
 # TODO: Move somewhere else
-from socialgraph.models import Profile
+from socialgraph.models import Profile, FollowRecommendations
 
 
 def create_profiles(data):
@@ -11,4 +11,14 @@ def create_profiles(data):
                     language=node.get('language'),
                     profile_pic=node.get('profile_pic') if node.get('profile_pic') is not None else 'default.jpg')
         p.save()
+    print("Updated Database!")
+
+def create_Recommendations(data):
+    FollowRecommendations.objects.all().delete()  # Delete all profile entries in the database.
+    for node in data['nodes']:
+        r = FollowRecommendations(layer = node.get('hopLayer'), bacnet_id=node.get('id'), name=node.get('name'), gender=node.get('gender'),
+                    birthday=node.get('birthday'), country=node.get('country'), town=node.get('town'),
+                    language=node.get('language'),
+                    profile_pic=node.get('profile_pic') if node.get('profile_pic') is not None else 'default.jpg')
+        r.save()
     print("Updated Database!")
