340a7e558707f89b5970f1cb62be19346ca92abd
pascalraphaelkunz <pasca.kunz@stud.unibas.ch>
2021-05-07 08:50:16 +0200

    add class FollowRecommendations in models.py

--- 21-fs-ias-lec/FrontEnd/socialgraph/models.py
+++ 21-fs-ias-lec/FrontEnd/socialgraph/models.py
@@ -31,3 +31,34 @@ class Profile(models.Model):
         if self.language is not None:
             details['Language']= self.language
         return details
+
+class FollowRecommendations(models.Model):
+    layer = models.IntegerField(default=None)
+    bacnet_id = models.CharField(max_length=64, primary_key=True)
+    name = models.CharField(max_length=64)
+    gender = models.CharField(max_length=6, blank=True, null=True, default=None)
+    birthday = models.DateField(blank=True, null=True, default=None)
+    country = models.CharField(max_length=64, blank=True, null=True, default=None)
+    town = models.CharField(max_length=64, blank=True, null=True, default=None)
+    language = models.CharField(max_length=256, blank=True, null=True,
+                                default=None)  # https://stackoverflow.com/questions/22340258/django-list-field-in-model
+    profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.name} Profile / {self.bacnet_id}'
+
+    def get_details(self):
+        details = {}
+        if (self.layer < 3):
+            if self.gender is not None:
+                details['Gender'] = self.gender
+            if self.birthday is not None:
+                details['Birthday'] = self.birthday
+            if self.country is not None:
+                details['Country'] = self.country
+            if self.town is not None:
+                details['Town'] = self.town
+            if self.language is not None:
+                details['Language'] = self.language
+        return details
+
