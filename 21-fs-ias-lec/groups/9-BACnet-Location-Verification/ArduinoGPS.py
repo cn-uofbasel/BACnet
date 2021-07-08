e5ee52683613781e94a6b319443406cf0e4d10ab
rasmuswilli <rasmus.willi@stud.unibas.ch>
2021-07-08 13:29:40 +0200

    port will be chosen automatically on macos and windows

--- 21-fs-ias-lec/groups/9-BACnet-Location-Verification/ArduinoGPS.py
+++ 21-fs-ias-lec/groups/9-BACnet-Location-Verification/ArduinoGPS.py
@@ -1,13 +1,30 @@
 import serial
 import threading
 import time
+import serial.tools.list_ports
+import platform
 
-ser = serial.Serial('/dev/tty.usbmodem11401', 9600, timeout=1)
-#USB PORT FOR MAC (varies when reconnected...)
-#WINDOWS WOULD MOST LIKELY BE 'COM3'
+ports = list(serial.tools.list_ports.comports())
+port = None
+
+if "macOS" in platform.platform():
+    
+    for p in ports:
+        if "usbmodem" in p.name:
+            port = '/dev/' + str(p.name)
+            break
+
+
+elif "Windows" in platform.platform():
+    for p in ports:
+        if "USB Serial Device" in p.description:
+            port = str(p.device)
+            break
+
+
+ser = serial.Serial(port, 9600, timeout=1)
 
 uid, latitude, longitude = None, None, None
-
 
 class serialReadingThread(threading.Thread):
     def __init__(self, iD, name):
