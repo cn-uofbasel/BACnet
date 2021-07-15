3df0c14c7e74704e2ff164985504f377abf80db3
rasmuswilli <rasmus.willi@stud.unibas.ch>
2021-07-15 11:15:48 +0200

    add comments

--- 21-fs-ias-lec/groups/9-BACnet-Location-Verification/Demo/ArduinoGPS.py
+++ 21-fs-ias-lec/groups/9-BACnet-Location-Verification/Demo/ArduinoGPS.py
@@ -1,8 +1,8 @@
-import platform
+import platform # used to determine operating system
 import sys
 import threading
 import time
-import serial.tools.list_ports
+import serial.tools.list_ports # used to list open ports
 from logMerge import LogMerge
 from logMerge.PCAP import PCAP
 from logMerge.eventCreationTool import EventCreationTool
@@ -14,26 +14,28 @@ sys.path.append(".BACnet/demo/lib")
 # TODO: delete local feed if needed?
 # TODO: if local feed is already stored, overwrite and dont initialize a new one
 
+# Lists all ports currently open.
 ports = list(serial.tools.list_ports.comports())
 port = None
 
+# macOS has its ports stored in the /dev/ directors
 if "macOS" in platform.platform():
-
     for p in ports:
         if "usbmodem" in p.name:
             port = '/dev/' + str(p.name)
             break
 
-
+# Windows uses COM ports.
 elif "Windows" in platform.platform():
     for p in ports:
         print(p.description, p.name)
         if "USB Serial Device" in p.description:
             port = str(p.device)
             break
-# TODO: change Port automatic
+# TODO: change Port automatically
 port = "COM3"
 
+# Initialize serial connection with chosen port
 ser = serial.Serial(port, 9600, timeout=1)
 
 # initialize uid, lat, long
@@ -48,14 +50,18 @@ first_event = ecf.first_event("verificationTool", master_feed_id)
 
 
 class SerialReadingThread(threading.Thread):
+
+    # standard thread __init__
     def __init__(self, iD, name):
         threading.Thread.__init__(self)
         self.iD = iD
         self.name = name
 
     def run(self):
+        # access global variables for further processing
         global uid, latitude, longitude
         while True:
+            #for each serial line, check for type of information
             data_raw = ser.readline().decode().strip()
             if data_raw.startswith("x1uid"):
                 uid = data_raw[6:]
